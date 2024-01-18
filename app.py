e2763263f4f53adaa02dd9eb7883df16187dac98
Vitor Pinto Canadas <vitorpinto@mbp-de-vitor.home>
2024-01-18 16:20:15 +0100

    logo espace

--- app.py
+++ app.py
@@ -823,9 +823,15 @@ def espace_utilisateur():
 
     # Sélectionner les paris de l'utilisateur
     select_mises_query = '''
-        SELECT mises.id, matchs.equipe1, matchs.equipe2, matchs.jour, matchs.debut, matchs.fin, mises.mise1, mises.mise2, mises.resultat1, mises.resultat2, matchs.statut, matchs.vainqueur, mises.equipe1, mises.equipe2, mises.datemise
+        SELECT 
+            mises.id, matchs.equipe1, matchs.equipe2, matchs.jour, matchs.debut, matchs.fin, mises.mise1, mises.mise2, mises.resultat1, mises.resultat2, matchs.statut, matchs.vainqueur, mises.equipe1, mises.equipe2, mises.datemise,
+            e1.logo AS logo_equipe1, e2.logo AS logo_equipe2
         FROM mises
         JOIN matchs ON mises.id_match = matchs.id
+        LEFT JOIN 
+            equipes e1 ON matchs.equipe1 = e1.nom_equipe
+        LEFT JOIN 
+            equipes e2 ON matchs.equipe2 = e2.nom_equipe
         WHERE mises.id_utilisateur = %s 
         ORDER BY 
             CASE matchs.statut 
@@ -833,7 +839,9 @@ def espace_utilisateur():
                 WHEN 'Terminé' THEN 2
                 WHEN 'À venir' THEN 3
                 ELSE 4
-            END
+            END,
+            matchs.jour
+
     '''
     cursor.execute(select_mises_query, (id_utilisateur,))
     mises = cursor.fetchall()
