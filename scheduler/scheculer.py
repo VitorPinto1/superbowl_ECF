68401199af7a9bbc3a4364f7a0e82b75ff1768eb
vitor pinto <vitor.pinto@ecoles-epsi.net>
2025-01-13 16:04:00 +0100

    scheduler

--- scheduler/scheculer.py
+++ scheduler/scheculer.py
@@ -1,15 +1,14 @@
 from flask_apscheduler import APScheduler
 
-scheduler = APScheduler()  # Crear una instancia del planificador
+scheduler = APScheduler()  
 
 def taches():
-    """Registrar tareas planificadas aquí."""
-    from match.match import generer_matchs_quotidiens  # Importación local para evitar ciclos
+    from match.match import generer_matchs_quotidiens  
 
     scheduler.add_job(
         id='generer_matchs_quotidiens',
         func=generer_matchs_quotidiens,
         trigger='cron',
-        hour=15,  # Configura la hora de ejecución
-        minute=51
+        hour=0,  
+        minute=0
     )
