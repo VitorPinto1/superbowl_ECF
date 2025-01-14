276dfd2ff6f220e8c90dcceaf60a325f1bf9167c
vitor pinto <vitor.pinto@ecoles-epsi.net>
2025-01-14 15:03:31 +0100

    €

--- api/app.py
+++ api/app.py
@@ -4,7 +4,7 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from api.config import *
-from scheduler.scheculer import *
+from scheduler.scheduler import *
 
 
 env = os.getenv('FLASK_ENV', 'development')
